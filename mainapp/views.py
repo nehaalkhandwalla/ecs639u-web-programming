4821d33ac5d2ab96e627f6e81fe43d5adb2a6f92
Ragnar Castillo <lormidorido@qmul.ac.uk>
2020-11-07 19:42:36 +0000

    health probe command added

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def hello(request):
     return render(request, 'mainapp/index.html', {})
+
+# health probe
+def health(request):
+    """Takes an request as a parameter and gives the count of pageview objects as reponse"""
+    return HttpResponse(PageView.objects.count())
