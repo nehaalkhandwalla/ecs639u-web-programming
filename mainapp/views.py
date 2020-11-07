c21bad19d4802c2eb749bf2abf14e6cdd54dc25a
Ragnar Castillo <lormidorido@qmul.ac.uk>
2020-11-07 20:43:12 +0000

    yet another probe test

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import PageView
-
 # Create your views here.
 
 def hello(request):
     return render(request, 'mainapp/index.html', {})
 
 def health(request):
-    """Takes an request as a parameter and gives the count of pageview objects as reponse"""
-    return HttpResponse(PageView.objects.count())
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
