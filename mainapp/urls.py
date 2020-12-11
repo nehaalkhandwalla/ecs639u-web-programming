73c8093d2140c27ea511873214889c7ded685d85
Ragnar Castillo <lormidorido@qmul.ac.uk>
2020-12-11 03:06:38 +0100

    5

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from .views import hello
+from .views import simple_upload
 
 urlpatterns = [
     path('', hello),
-    url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
+    url(r'^uploads/simple/$', simple_upload, name='simple_upload'),
 ]
