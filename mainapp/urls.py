c6fe32b652b673e902f4c89153c5801378ba2799
Ragnar Castillo <lormidorido@qmul.ac.uk>
2020-12-11 03:41:23 +0100

    14

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
+from django.conf import settings
 from django.conf.urls.static import static
 
 from .views import hello
@@ -8,6 +9,6 @@ from .views import simple_upload
 urlpatterns = [
     path('', hello),
     url(r'^uploads/simple/$', simple_upload, name='simple_upload'),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
