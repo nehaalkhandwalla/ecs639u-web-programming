7a1ec4a6fa3642a395d3d1fe2e535a3024fa7d44
Ragnar Castillo <lormidorido@qmul.ac.uk>
2020-12-11 03:44:23 +0100

    16

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,6 +9,5 @@ from .views import simple_upload
 urlpatterns = [
     path('', hello),
     url(r'^uploads/simple/$', simple_upload, name='simple_upload'),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
