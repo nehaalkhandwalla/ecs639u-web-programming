c6a0078a633153a61236e68631df4b18fe18267f
Ragnar Castillo <lormidorido@qmul.ac.uk>
2020-12-11 06:54:28 +0100

    20

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', hello),
     url(r'^uploads/simple/$', simple_upload, name='simple_upload'),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
